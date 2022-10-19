d15e90938d3d824f48df8b623c120181b981ac4a
Viktor Triber <vtriber@ya.ru>
2022-10-19 10:47:49 +0300

    Добавлен exercise2.sql

---
+++ exercise2.sql
@@ -0,0 +1,53 @@
+CREATE TABLE IF NOT EXISTS genres (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(40) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS performers (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(40) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS albums (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(40) NOT NULL,
+	year INTEGER NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS tracks (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(40) NOT NULL,
+	duration INTEGER NOT NULL,
+	album_id INTEGER NOT null REFERENCES albums(id)
+);
+
+CREATE TABLE IF NOT EXISTS compilations (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(40) NOT NULL,
+	year INTEGER NOT null
+);
+
+CREATE TABLE IF NOT EXISTS perf_gen (
+	genre_id INTEGER REFERENCES genres(id),
+	performer_id INTEGER REFERENCES performers(id),
+	CONSTRAINT gp PRIMARY KEY (genre_id, performer_id)
+);
+
+CREATE TABLE IF NOT EXISTS perf_album (
+	performer_id INTEGER REFERENCES performers(id),
+	album_id INTEGER REFERENCES albums(id),
+	CONSTRAINT pa PRIMARY KEY (performer_id, album_id)
+);
+
+CREATE TABLE IF NOT EXISTS track_compil (
+	track_id INTEGER REFERENCES tracks(id),
+	compilation_id INTEGER REFERENCES compilations(id),
+	CONSTRAINT tc PRIMARY KEY (track_id, compilation_id)
+);
+
+
+
+
+
+
+
